6ce07d05ae1f7f896d0987901e9cf92722353fdf
thehiddenauto <thehiddenauto@gmail.com>
2025-08-15 21:17:39 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import App from './App.jsx';
+import app from './app.jsx';
 import './index.css';
 
 // Remove loading spinner when React mounts
