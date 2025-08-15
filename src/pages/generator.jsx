c56edf529ae9e268bcd8419c2204cb90d358070e
thehiddenauto <thehiddenauto@gmail.com>
2025-08-15 21:49:18 +0200

    Delete src/pages/generator.jsx

--- src/pages/generator.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import VideoGenerator from './VideoGenerator';
-
-// Re-export VideoGenerator as Generator for backward compatibility
-const Generator = (props) => {
-  return <VideoGenerator {...props} />;
-};
-
-export default Generator;
