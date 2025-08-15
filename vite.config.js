2d660d2ea74c2957b313d39dadaf69e396e3e4d8
thehiddenauto <thehiddenauto@gmail.com>
2025-08-15 19:02:17 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
-    sourcemap: false, // Disable sourcemaps in production
+    sourcemap: false,
     minify: 'terser',
     target: 'es2015',
     rollupOptions: {
@@ -43,6 +43,24 @@ export default defineConfig({
     port: 3000,
     open: true,
     cors: true,
+    proxy: {
+      '/api': {
+        target: 'https://backendnew-lz1u.onrender.com',
+        changeOrigin: true,
+        secure: true,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      }
+    }
   },
   preview: {
     port: 8080,
